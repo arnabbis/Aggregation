51c02ed19e9ac8b3181fd91fe35be65fb42d1a0a
arnabbis <biswas.anuska.2004@gmail.com>
2024-02-11 22:12:53 +0530

    feat: product module to add product

---
+++ src/route/productRoute.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controller/productController');
+router.post('/createProduct', productController.createProduct);
+// router.get('/getAllProducts', productController.getAllProducts);
+module.exports = router
