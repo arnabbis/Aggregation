e91f3a81930d0daa07c9a141bc41994f42e0de93
arnabbis <biswas.anuska.2004@gmail.com>
2024-02-18 20:34:50 +0530

    feat: added api

--- src/route/productRoute.js
+++ src/route/productRoute.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controller/productController');
 router.post('/createProduct', productController.createProduct);
-// router.get('/getAllProducts', productController.getAllProducts);
+router.get('/getAllProductsByCategory', productController.getAllProductsbyCategory);
+router.get('/getAllProductsByPrice', productController.getProductByPrice);
 module.exports = router
