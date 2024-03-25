0e63fb605cc0b5491075a9da2b34003803d5de80
arnabbis <biswas.anuska.2004@gmail.com>
2024-03-25 11:19:29 +0530

    feat: added changes to the code

--- src/route/productRoute.js
+++ src/route/productRoute.js
@@ -4,4 +4,7 @@ const productController = require('../controller/productController');
 router.post('/createProduct', productController.createProduct);
 router.get('/getAllProductsByCategory', productController.getAllProductsbyCategory);
 router.get('/getAllProductsByPrice', productController.getProductByPrice);
+router.get('/getAllActiveProducts', productController.getProductWhichAreActive);
+router.get('/getAllProductByQuantity', productController.getAllProductByQuantity);
+router.get('/getAllProductsCountByCategory', productController.getAllProductsByCategoryCount);
 module.exports = router
